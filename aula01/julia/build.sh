3213ccceacbf55120b8af936100c2cc20de2a1a9
juliakoch <ejuliakoch@gmail.com>
2017-08-19 16:27:04 +0000

    AUla 02 - Concluidoy

--- aula01/julia/build.sh
+++ aula01/julia/build.sh
@@ -1,3 +1,10 @@
-docker rmi -f mysql-docker || true
-docker build -t mysql-docker  .
 
+docker rmi -f tomee-soujava
+docker rmi -f mysql-soujava
+
+cp ../apptest/target/apptest-1.0.war apptest.war
+
+docker build -t tomee-soujava .
+
+cd mysql
+docker build -t mysql-soujava .
--- aula01/julia/run.sh
+++ aula01/julia/run.sh
@@ -1,5 +1,15 @@
-docker rm -f mysql-docker || true
-docker rm -f host1 || true
-docker run -d --name mysql-docker -h mysql-docker -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=dockermysql  mysql-docker
-sleep 20
-docker run -d --name host1 -h host1 -p 8080:8080 --link  mysql-docker:mysql-docker tomee-war
+docker rm -f $(docker ps -a -q)
+
+docker run -d --name mysql-soujava \
+	-h mysql-soujava \
+	-p 3306:3306 \
+	-e MYSQL_ROOT_PASSWORD=soujava \
+	-e MYSQL_DATABASE=soujava \
+	mysql-soujava
+
+sleep 3
+
+docker run -d --name tomee-soujava \
+	-h tomee-soujava \
+	--link mysql-soujava:mysql-soujava \
+-p 8080:8080 tomee-soujava
