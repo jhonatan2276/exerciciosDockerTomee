d399828b2c7a3aedc0ee7e4a20acc2b3835d86de
Elder Moraes <elder.moraes@gmail.com>
2017-09-04 14:27:28 -0300

    Ajustes

--- aula01/julianoferro/mysql/database.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `unidavi`.`access` (
-  `idaccess` INT(8) NOT NULL AUTO_INCREMENT,
-  `description` VARCHAR(500) NOT NULL,
-  PRIMARY KEY (`idaccess`));
--- aula01/julianoferro/run.sh
+++
@@ -1,2 +0,0 @@
- sudo docker run --name mysql -e MYSQL_ROOT_PASSWORD=123 -d mysql:5.6
- sudo docker run --name tomee --link mysql:mysql -p 9000:80 -d tomee:8-jre-1.7.4-webprofile
